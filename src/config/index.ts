8861a56ead5e562a04b88efd8978e2a401545db7
liuyun <liuyun.dev@qq.com>
2024-02-29 15:52:26 +0800

    feat(base): 完成项目环境变量读取基本逻辑；

---
+++ src/config/index.ts
@@ -0,0 +1,61 @@
+import type { App } from "vue";
+
+let config: object = {};
+const { VITE_PUBLIC_PATH } = import.meta.env;
+
+/**
+ * 设置全局配置
+ * @param cfg
+ */
+const setConfig = (cfg?: unknown) => {
+  config = Object.assign(config, cfg);
+};
+
+/**
+ * 获取项目动态全局配置，如果有key则获取某个属性的值
+ * @param [key]
+ */
+const getConfig = (key?: string): PlatformConfigs => {
+  if (typeof key === "string") {
+    const arr = key.split(".");
+    if (arr && arr.length) {
+      let data = config;
+      arr.forEach(v => {
+        if (data && typeof data[v] !== "undefined") {
+          data = data[v];
+        } else {
+          data = null;
+        }
+      });
+      return data;
+    }
+  }
+  return config;
+};
+
+/** 获取项目动态全局配置 */
+export const getPlatformConfig = async (app: App): Promise<undefined> => {
+  app.config.globalProperties.$config = getConfig();
+  // fetch发送请求获取，也可以使用axios
+  return fetch(`${VITE_PUBLIC_PATH}platform-config.json`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then(config => {
+      let $config = app.config.globalProperties.$config;
+      // 自动注入项目配置
+      if (app && $config && typeof config === "object") {
+        $config = Object.assign($config, config);
+        app.config.globalProperties.$config = $config;
+        // 设置全局配置
+        setConfig($config);
+      }
+      return $config;
+    })
+    .catch(() => {
+      throw new Error("请在public文件夹下添加platform-config.json配置文件");
+    });
+};
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
-// import './assets/main.css'
-
 import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { setupStore } from "@/store";
+import { getPlatformConfig } from "./config";
 import App from "./App.vue";
 import router from "./router";
-import "@unocss/reset/normalize.css";
+import "./styles/index.scss";
 import "virtual:uno.css";
 const app = createApp(App);
 
-app.use(createPinia());
-app.use(router);
-
-app.mount("#app");
+getPlatformConfig(app).then(async config => {
+  console.log("config", config);
+  // 先初始化store，再挂载路由，否则在路由守卫中使用store会报错
+  setupStore(app);
+  app.use(router);
+  await router.isReady();
+  // injectResponsiveStorage(app, config);
+  // app
+  //   .use(MotionPlugin)
+  //   .use(useI18n)
+  //   .use(useElementPlus)
+  //   .use(Table)
+  //   .use(PureDescriptions)
+  //   .use(useEcharts);
+  app.mount("#app");
+});
